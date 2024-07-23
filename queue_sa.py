f01df68d3721ad59f601d0768422be0370af3293
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-07-22 20:03:16 -0700

    Update queue_sa.py

--- queue_sa.py
+++ queue_sa.py
@@ -1,1 +1,29 @@
+# Name: Jaskaran Singh Sidhu
+# OSU Email: sidhuja@gmail.com
+# Course: CS261 - Data Structures
+# Assignment: 3
+# Due Date: 22 July
+# Description: Implementation of a queue using a static array.
 
+class StaticArray:
+    def __init__(self, size=4):
+        self.size = size
+        self.array = [None] * size
+
+    def __str__(self):
+        return str(self.array)
+
+    def length(self):
+        return self.size
+
+class QueueException(Exception):
+    pass
+
+class Queue:
+    def __init__(self):
+        self._sa = StaticArray(4)
+        self._front = 0
+        self._back = -1
+        self._current_size = 0
+
+    def
